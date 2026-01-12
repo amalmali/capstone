292a484619e1aa5f2b44ba95eb8ab48c844b7fba
ibtesamalmarashi <almarashi2016@gmail.com>
2026-01-12 17:15:25 +0300

    Add files via upload

---
+++ last/requirements.py
@@ -0,0 +1,23 @@
+# Web Framework & Server
+fastapi
+uvicorn
+jinja2
+python-multipart
+
+# AI & RAG (LangChain & VectorStore)
+langchain
+langchain-community
+sentence-transformers
+faiss-cpu
+pypdf
+
+# Speech to Text (STT)
+vosk
+sounddevice
+
+# Text to Speech (TTS) & Audio Playback
+edge-tts
+pygame
+
+# Utilities
+pathlib
